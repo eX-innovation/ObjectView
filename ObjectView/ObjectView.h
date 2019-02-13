b94903552cc46c6ebd1d0c51652a1f333983b6f1
Michael Mayerhofer <michael@4mayerhofers.de>
2019-02-13 19:55:06 +0100

    Initial Commit

---
+++ ObjectView/ObjectView.h
@@ -0,0 +1,19 @@
+//
+//  ObjectView.h
+//  ObjectView
+//
+//  Created by Michael Mayerhofer on 2/13/19.
+//  Copyright © 2019 Michael Mayerhofer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectView.
+FOUNDATION_EXPORT double ObjectViewVersionNumber;
+
+//! Project version string for ObjectView.
+FOUNDATION_EXPORT const unsigned char ObjectViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectView/PublicHeader.h>
+
+
